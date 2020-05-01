f4e03823d4d1d8419858093aaee24fe560f65edd
Fsociety <atirekkumar@MacBook-Pro.local>
2020-05-02 00:21:21 +0530

    Able to get blogspot links. Removed headless argument as it would take too long or cause the script to hang.

--- selenium-script.py
+++ selenium-script.py
@@ -5,28 +5,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import sys, os
+from datetime import datetime #for testing purpose
 
 def parse_url(url):
-	url = url + '&readType=1'
-	return url
+	if '&readType=1' in url:
+		url = url + '&readType=1'
+	return str(url)
 
 def extract_source_code(url):
+	# print("Selenium takes time upto 30 seconds or even a minute to fire up the browser. Please wait...")
 	#Create an instance of chrome
 	options = webdriver.ChromeOptions();
-	options.add_argument('headless'); #to not open a browser window
-	options.add_argument('--load-images=no');
+	# options.add_argument('headless'); #to not open a browser window
+	options.add_argument('--load-images=no'); #to lower loading time
 	browser = webdriver.Chrome(options=options)
 	browser.get(url)
 
 	# Wait 7 seconds for page to load
-	timeout = 7
+	timeout = 10	
 	try:
-    	WebDriverWait(browser, timeout)
+		element = WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.ID, "containerRoot")))
 	except TimeoutException:
-    	print("Timed out waiting for page to load")
-    	browser.quit()
+		print("Timed out waiting for page to load")
+		browser.quit()
 
-    #store source code in a .txt file
+	#store source code in a .txt file
 	source_Code = browser.page_source
 	soure_file = open('source.txt','w')
 	soure_file.write(source_Code)
@@ -34,13 +37,29 @@ def extract_source_code(url):
 	soure_file.close()
 	browser.quit()
 
+def extract_image_links():
+	links = []
+	with open("source.txt", "r") as a_file:
+		for line in a_file:	
+			if 'blogspot' in line:
+				if 'src' in line:
+					pass
+				else:
+					links.append(line.split('"')[1])	
+	return links			
+
 def main():
 	#accept link as argument
 	if len(sys.argv) < 2:
 		print("Enter url as argument in the command line")
 		sys.exit()
 	url = parse_url(sys.argv[1])
+	start = datetime.now()
 	extract_source_code(url) #stores source code of url in 'source.txt'
-	
-
+	links = extract_image_links() #stores links of all images in links list
+	# print(links)
+	finish = datetime.now() - start
+	print(finish)
+	for element in links:
+		print(element)
 main()
