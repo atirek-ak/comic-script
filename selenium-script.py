f6406148f9d409efe8567d53cad870f070a809b9
Fsociety <atirekkumar@MacBook-Pro.local>
2020-05-01 22:42:04 +0530

    Structured code

--- selenium-script.py
+++ selenium-script.py
@@ -6,45 +6,41 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import sys, os
 
-def parseUrl(url):
+def parse_url(url):
 	url = url + '&readType=1'
 	return url
 
-def main():
-	#accept link as argument
-	if len(sys.argv) < 2:
-		print("Enter url as argument in the command line")
-		sys.exit()
-	url = sys.argv[1]
-	url = parseUrl(url)
-
+def extract_source_code(url):
 	#Create an instance of chrome
-	# driver = webdriver.PhantomJS(service_args=['--load-images=no'])
 	options = webdriver.ChromeOptions();
-	options.add_argument('headless');
+	options.add_argument('headless'); #to not open a browser window
 	options.add_argument('--load-images=no');
 	browser = webdriver.Chrome(options=options)
+	browser.get(url)
 
-	# option.add_argument(" — incognito")
-	# browser = webdriver.Chrome(options=driver)
-	# browser = webdriver.PhantomJS(service_args=['--load-images=no'])
-	browser.get("https://readcomiconline.to/Comic/Constantine/Issue-6?id=1466&readType=1")
 	# Wait 7 seconds for page to load
 	timeout = 7
-	# try:
-    	# WebDriverWait(browser, timeout).until(
-				# EC.presence_of_element_located((By.ID, "divImage")))
-	# except TimeoutException:
-    	# print("Timed out waiting for page to load")
-    	# browser.quit()
+	try:
+    	WebDriverWait(browser, timeout)
+	except TimeoutException:
+    	print("Timed out waiting for page to load")
+    	browser.quit()
 
+    #store source code in a .txt file
 	source_Code = browser.page_source
 	soure_file = open('source.txt','w')
 	soure_file.write(source_Code)
 	soure_file.flush()
 	soure_file.close()
 	browser.quit()
-	# search_field = browser.findElement(By.ID, "divImage")
-	# print(search_field)    
+
+def main():
+	#accept link as argument
+	if len(sys.argv) < 2:
+		print("Enter url as argument in the command line")
+		sys.exit()
+	url = parse_url(sys.argv[1])
+	extract_source_code(url) #stores source code of url in 'source.txt'
+	
 
 main()
